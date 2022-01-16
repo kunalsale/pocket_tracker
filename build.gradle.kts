48a62147f2b74f567143ed24a439a541957738f0
kunalsale <kunal.sale94@gmail.com>
2022-01-16 21:22:52 +0530

    Compose configuration done, kotlin version - 1.5.31 and compose version - 1.0.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,13 @@
 buildscript {
+    val compose_version by extra("1.0.5")
     repositories {
         gradlePluginPortal()
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
-        classpath("com.android.tools.build:gradle:7.0.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+        classpath("com.android.tools.build:gradle:7.0.4")
     }
 }
 
