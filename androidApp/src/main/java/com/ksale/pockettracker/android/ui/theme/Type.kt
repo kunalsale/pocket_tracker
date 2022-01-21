f063f63f78163211051804969614c2c322aeb0a6
kunalsale <kunal.sale94@gmail.com>
2022-01-21 12:57:51 +0530

    Font set up done, Used defaultFontFamily for the entire app

--- androidApp/src/main/java/com/ksale/pockettracker/android/ui/theme/Type.kt
+++ androidApp/src/main/java/com/ksale/pockettracker/android/ui/theme/Type.kt
@@ -1,18 +1,19 @@
 package com.ksale.pockettracker.android.ui.theme
 
 import androidx.compose.material.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.ksale.pockettracker.android.R
 
+val OutfitFontFamily = FontFamily(
+    Font(R.font.outfit_regular),
+    Font(R.font.outfit_bold, FontWeight.Bold),
+    Font(R.font.outfit_medium, FontWeight.Medium),
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
+    defaultFontFamily = OutfitFontFamily,
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
